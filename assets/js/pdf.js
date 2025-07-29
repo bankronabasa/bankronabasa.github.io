7490244d8499b04993a8cc5c20cf58ca261dfaff
bankronabasa <bankronabasa2025@gmail.com>
2025-07-29 21:07:36 +0800

    update urutan triwulan di tahun 2025 awalnya V jadi IV

--- assets/js/pdf.js
+++ assets/js/pdf.js
@@ -35,7 +35,7 @@ const data = [
             },
             {
                 "tahun": 2025,
-                "triwulan": "V",
+                "triwulan": "IV",
                 "file": ["LKPK-LKP-01.pdf", "LKPK-LKP-02.pdf", "LKPK-LKP-03.pdf", "LKPK-LKP-04.pdf", "LKPK-LKP-05.pdf"]
             }
         ]
